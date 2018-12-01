5974a315bfead7219d94c67cf7d92ede2e3a402f
Sahadat Hossien <sahadatsays.ios@gmail.com>
2018-11-30 22:29:39 -0800

    add a empty table view and setup a empty table view custom cell prototype

---
+++ CheckList/CheckListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Sahadat  Hossain on 30/11/18.
+//  Copyright © 2018 Sahadat  Hossain. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        
+        return cell
+    }
+
+}
+
---
+++ CheckList/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  CheckList
+//
+//  Created by Sahadat  Hossain on 30/11/18.
+//  Copyright © 2018 Sahadat  Hossain. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UICollectionViewCell {
+    
+}
